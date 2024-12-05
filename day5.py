f62766faf7044263f67ce8317ce5f923d48d2738
CLARA-PANTOJA <clara.pantoja@wegrant.com>
2024-12-05 23:52:42 +0100

    day5 part2

--- day5.py
+++ day5.py
@@ -5,6 +5,11 @@ def find(lista, valor):
     except ValueError:
         return -1
 
+def middle_page(pages):
+        long = len(pages)
+        center_index = int(math.ceil((long-1)/2))
+        middle = int(pages[center_index])
+        return middle
 
 with open("input5.txt") as file:
     input_rules = []
@@ -18,31 +23,48 @@ with open("input5.txt") as file:
             input_order.append(r.split(","))
 
     nulas = []
+    corrections_nules = []
     for i, updates in enumerate(input_order):
+        rules_broke = 0
         for rule in input_rules:
             index_a = find(updates, rule[0])
             index_b = find(updates, rule[1])
             if index_a >= 0 and index_b >= 0:
                 if index_a > index_b:
                     nulas.append(i)
-                    break
-
+                    rules_broke += 1
+                    # break
+        if rules_broke > 0:
+            corrections_nules.append(rules_broke)
+    
     valid_updates = [element for i, element in enumerate(input_order) if i not in nulas]
-    #find middle page
+    invalid_updates = [element for i, element in enumerate(input_order) if i in nulas]
+
+
+    #part1
     sum_middle = 0
     for v_update in valid_updates:
-        long = len(v_update)
-        center_index = int(math.ceil((long-1)/2))
-        # print(f"long: {len(v_update)} -- medio: {center_index} -- valor medio: {v_update[center_index]}")
-        # print(f"v_update: {v_update}")
-        
-        # print(f"medio:{int(v_update[center_index])}")
-        sum_middle += int(v_update[center_index])
+        sum_middle += middle_page(v_update)
 
+    #part2
+    for j, invalid_up in enumerate(invalid_updates):
+        for k in range(corrections_nules[j]):
+            for rule in input_rules:
+                index_a = find(invalid_up, rule[0])
+                index_b = find(invalid_up, rule[1])
+                if index_a >= 0 and index_b >= 0:
+                    if index_a > index_b:
+                        temp = invalid_up[index_a]
+                        invalid_up[index_a] = invalid_up[index_b]
+                        invalid_up[index_b] = temp
+                        break
+    
+    sum_middle_invalid = 0
+    for in_update in invalid_updates:
+        middle = middle_page(in_update)
+        sum_middle_invalid += middle
 
-    print(f"part1: {sum_middle}")        
-    # print(f"input_order: {input_order}")
-    # print(f"nulas: {nulas}")
-    # print(f"validas: {valid_updates}")
+    print(f"part1: {sum_middle}")
+    print(f"part2: {sum_middle_invalid}")
 
 
